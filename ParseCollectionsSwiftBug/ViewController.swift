aff9a86d8ce21b9beac4adefe0dd72a53b14fff8
Kamil Burczyk <kamil.burczyk@gmail.com>
2014-11-11 12:30:48 +0100

    Initial Commit

---
+++ ParseCollectionsSwiftBug/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParseCollectionsSwiftBug
+//
+//  Created by Kamil Burczyk on 11.11.2014.
+//  Copyright (c) 2014 Sigmapoint. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
